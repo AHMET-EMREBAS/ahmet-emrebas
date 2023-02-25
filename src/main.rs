e52db8c07c524f108ea78264ab1a469c53de73f7
unknown <aemrebas.dev@gmail.com>
2023-02-25 16:40:04 -0600

    some

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args().skip(1);
+
+    let first = args.next().expect("First arguments is not provided!");
+
+    let second = args.next().expect("Second argument is required!");
+
+    println!("Key: {key} and Value: {value}", key = first, value = second);
 }
